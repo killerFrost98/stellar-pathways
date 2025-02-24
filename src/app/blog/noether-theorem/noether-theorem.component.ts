0675c60df431299b63aaee2823b102c8c16fe740
killerFrost98 <phanidavuluru98@gmail.com>
2025-02-25 00:14:11 +0530

    Revert "Auto-generated commit"
    This reverts commit 53ac725af37debd5ee8485510262c4c7e9eb4357.

---
+++ src/app/blog/noether-theorem/noether-theorem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noether-theorem',
+  standalone: true,
+  imports: [],
+  templateUrl: './noether-theorem.component.html',
+  styleUrl: './noether-theorem.component.scss'
+})
+export class NoetherTheoremComponent {
+
+}
