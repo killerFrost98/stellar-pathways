bea8ddf5f814f2da3a991b380ee022beb568438c
killerFrost98 <phanidavuluru98@gmail.com>
2025-04-06 02:48:05 +0530

    Removed unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,20 +7,8 @@ export const routes: Routes = [
       .then(m => m.SolarSystemModule)
   },
   {
-    path: 'noether-theorem',
-    loadComponent: () => import('./blog/noether-theorem/noether-theorem.component').then(m => m.NoetherTheoremComponent)
-  },
-  {
-    path: 'cpu-architecture',
-    loadComponent: () => import('./blog/cpu-micro-architecture/cpu-micro-architecture.component').then(m => m.CpuMicroArchitectureComponent)
-  },
-  {
-    path: 'immune-system',
-    loadComponent: () => import('./blog/immune-system/immune-system.component').then(m => m.ImmuneSystemComponent)
-  },
-  {
     path: 'blog',
-    loadComponent: () => import('./blog/blog-component/blog.component').then(m => m.BlogComponent)
+    loadComponent: () => import('./blog/blog.component').then(m => m.BlogComponent)
   },
   {
     path: '**',
